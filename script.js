f4d0bb4ce8cbc52137c7aec0382c3d1926b93662
TeknoTejas <tejas.salunke@teknopoint.in>
2023-04-11 16:52:10 +0530

    timer added

--- script.js
+++ script.js
@@ -3,13 +3,27 @@ let timer = document.querySelector('p');
 function getMin(minToSec) {
     minToSec -= 1;
     let sec = 60;
-        setInterval(function () {
-            sec -= 1;
+    setInterval(function () {
+        sec -= 1;
+        timer.innerText = minToSec * 1 + ":" + sec;
+        let secLength = sec.toString().split("").length;
+        let minLength = minToSec.toString().split("").length;
+        if (minLength == 1 && secLength == 1) {
+            timer.innerText = "0" + minToSec * 1 + ":" + "0" + sec;
+        }
+        if (minLength != 1 && secLength != 1) {
             timer.innerText = minToSec * 1 + ":" + sec;
-            if(sec == 0){
-                minToSec -= 1;
-                sec = 60;
-            }
-        }, 1000);
+        }
+        if (minLength == 1 && secLength != 1) {
+            timer.innerText = "0" + minToSec * 1 + ":" + sec;
+        }
+        if (minLength != 1 && secLength == 1) {
+            timer.innerText = minToSec * 1 + ":" + "0" + sec;
+        }
+        if (sec == 0) {
+            minToSec -= 1;
+            sec = 60;
+        }
+    }, 1000);
 }
 getMin(prompt("Enter munites"))
